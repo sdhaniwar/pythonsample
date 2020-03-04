3a02104ca7b0fdf2532435e10aa5243c9aa770f9
sdhaniwar@gmail.com <37060403+sdhaniwar@users.noreply.github.com>
2020-03-05 00:36:27 +0530

    new changes applied

--- webapp.py
+++ webapp.py
@@ -1,5 +1,14 @@
+from flask import Flask;
+
+
 def display():
     print("Digital1")
     print("Mainframe")
     print("Healthcare")
     print("Java")
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
