7342b32c0ebab4561fcdbf56e9e80abd8fcd409d
sdhaniwar@gmail.com <37060403+sdhaniwar@users.noreply.github.com>
2020-03-05 21:49:24 +0530

    new up

--- webapp.py
+++ webapp.py
@@ -6,6 +6,8 @@ import re
 #if entrypoint is not defined, app engine will look for an app
 app = Flask(__name__)
 
+SortedCap = []
+
 Feelings = ["Happiness","Confused","Fear","Anger","Surprise"]
 
 Captions = ["The only joy in the world is to begin.",
@@ -16,11 +18,18 @@ Captions = ["The only joy in the world is to begin.",
 
 
 for i in range(0, len(Captions)):
-    hap = re.search(".Happ.",Captions[i])
+    hap = re.search("Happ*|joy", Captions[i])
+    print(hap)
+    if(hap):
+        SortedCap.append(Captions[i])
+
+for j in range(0,len(SortedCap)):
+    print(SortedCap[j])
 
 @app.route('/')
 def display():
-    return render_template("index.html", len1 = len(Feelings), Feelings = Feelings, len2 = len(hap) , hap = hap)
+    return render_template("index.html", len1 = len(Feelings), Feelings = Feelings, len2 = len(SortedCap),
+                SortedCap = SortedCap)
 
 #if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
