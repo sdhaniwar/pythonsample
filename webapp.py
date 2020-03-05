8a293fb2daaf8b2cefa6d79b307ac3bc40b04608
sdhaniwar@gmail.com <37060403+sdhaniwar@users.noreply.github.com>
2020-03-05 19:42:39 +0530

    run

--- webapp.py
+++ webapp.py
@@ -15,4 +15,4 @@ def display():
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(use_reloader = True, debug=True)
+app.run(use_reloader = True, debug=True)
