b35d16e13e4f563051de007cf405f6885c064c45
sdhaniwar@gmail.com <37060403+sdhaniwar@users.noreply.github.com>
2020-03-05 20:33:22 +0530

    new update

--- webapp.py
+++ webapp.py
@@ -1,15 +1,26 @@
 
 from flask.app import Flask
 from flask import render_template
+import re
 
 #if entrypoint is not defined, app engine will look for an app
 app = Flask(__name__)
 
 Feelings = ["Happiness","Confused","Fear","Anger","Surprise"]
 
+Captions = ["The only joy in the world is to begin.",
+            "Happiness depends upon ourselves.",
+            "Happy people plan actions, they don’t plan results.",
+            "Courage is knowing what not to fear.",
+            "Find out what you’re afraid of and go live there."]
+
+
+for i in range(0, len(Captions)):
+    hap = re.search(".Happ.",Captions[i])
+
 @app.route('/')
 def display():
-    return render_template("index.html", len = len(Feelings), Feelings = Feelings)
+    return render_template("index.html", len = len(Feelings), Feelings = Feelings, hap = hap)
 
 #if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
