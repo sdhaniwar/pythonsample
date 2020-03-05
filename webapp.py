faaac764a35b603c8986e5da60352c571e7f2794
sdhaniwar@gmail.com <37060403+sdhaniwar@users.noreply.github.com>
2020-03-05 19:19:04 +0530

    new changes

--- webapp.py
+++ webapp.py
@@ -1,16 +1,17 @@
 
-from flask.app import Flask
+from flask.app import Flask, render_template
 
 #if entrypoint is not defined, app engine will look for an app
 app = Flask(__name__)
 
+Cloudplatforms = ["Amazon Web Services","Microsoft Azure","Google Cloud Platform","VMware"]
+
 @app.route('/')
 def display():
-    return 'Hey you!!'
-    print("You know i m here!")
+    return render_template("index.html", len = len(Cloudplatforms), Cloudplatforms = Cloudplatforms)
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(use_reloader = True, debug=True)
