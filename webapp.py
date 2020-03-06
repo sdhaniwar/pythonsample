bb36df4889b444ff3aa2d9acbd203691f610575b
sdhaniwar@gmail.com <37060403+sdhaniwar@users.noreply.github.com>
2020-03-06 08:54:00 +0530

    test

--- webapp.py
+++ webapp.py
@@ -6,6 +6,7 @@ import re
 #if entrypoint is not defined, app engine will look for an app
 app = Flask(__name__)
 
+SortedCap = []
 Sentiments = ["Happiness","Confused","Fear","Anger","Surprise"]
 
 Captions = ["The only joy in the world is to begin.",
@@ -14,28 +15,31 @@ Captions = ["The only joy in the world is to begin.",
             "Courage is knowing what not to fear.",
             "Find out what you’re afraid of and go live there."]
 
+for i in range(0, len(Captions)):
+    hap = re.search("Happ|joy", Captions[i])
+    if(hap):
+        SortedCap.append(Captions[i])
+
 @app.route('/')
 def display():
-    return render_template("index.html", len = len(Sentiments), Sentiments = Sentiments)
+    return render_template("index.html", len = len(Sentiments), Sentiments = Sentiments, len1 = len(SortedCap, SortedCap = SortedCap) )
 
-@app.route('/checkF')
-def checkF(feel):
-    SortedCap = []
-    if(feel=="Happiness"):
-        for i in range(0, len(Captions)):
-            hap = re.search("Happ|joy", Captions[i])
-            if(hap):
-                SortedCap.append(Captions[i])
-        print(SortedCap[i])
-    elif(feel=="Fear"):
-        for j in range(0,len(SortedCap)):
-            fear = re.search("fear",Captions[j])
-            if(fear):
-                if(SortedCap):
-                    SortedCap = []
-                    SortedCap.append(Captions[i])
-        print(SortedCap[i])
-    return render_template("result.html", SortedCap = SortedCap)
+#@app.route('/checkF')
+#def checkF(feel):
+ #   SortedCap = []
+#    if(feel=="Happiness"):
+#        for i in range(0, len(Captions)):
+#            hap = re.search("Happ|joy", Captions[i])
+#            if(hap):
+#                SortedCap.append(Captions[i])
+#    elif(feel=="Fear"):
+#        for j in range(0,len(SortedCap)):
+#            fear = re.search("fear",Captions[j])
+#            if(fear):
+#                if(SortedCap):
+#                    SortedCap = []
+#                    SortedCap.append(Captions[i])
+#    return render_template("result.html", SortedCap = SortedCap)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
